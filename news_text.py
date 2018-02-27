c816022b5c4df4a2fd29301dfca51cae917a9954
Emily J <Emily@Emilys-MacBook-Pro-89.local>
2018-02-27 01:02:54 -0500

    text prompts for file_name and reads txt file

--- news_text.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from newspaper import Article
-url = "https://www.nytimes.com/2018/01/30/us/politics/fact-check-sotu.html"
-
-article = Article(url)
-article.download()
-article.parse()
-
-print(article.text)
